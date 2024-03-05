da4848803f4fd599be2b7cca0a97bebb91fff4f6
omkhodse18 <omkarkhodse.18@gmail.com>
2024-03-05 11:35:14 +0530

    Created Spinner component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Filter from "./components/Filter";
 import Cards from "./components/Cards";
+import {apiUrl, filterData} from "./data"
+import { toast } from "react-toastify";
+import Spinner from "./components/Spinner";
 
 const App = () => {
+  let Toast = toast;
+  
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchData(){
+
+    setLoading(true);
+
+    try {
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      setCourses(output);
+    } catch (error) {
+      Toast.error("Something went wrong");
+      // console.log("Something went wrong");
+    }
+
+    setLoading(false);
+  }
+
+  useEffect( () => fetchData(), []);
 
   return (
   <div>
@@ -13,11 +38,13 @@ const App = () => {
     </div>
 
     <div>
-      <Filter></Filter>
+      <Filter filterData={filterData}></Filter>
     </div>
 
     <div>
-      <Cards></Cards>
+      {
+        loading ? (<Spinner></Spinner>) : (<Cards></Cards>)
+      }
     </div>
 
   </div>);
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-function Filter() {
-  return (
-    <div>
-      
-    </div>
-  )
+function Filter({filterData}) {
+
+    // let filterData = props.filterData;
+
+    return (
+        <div>
+            {
+                filterData.map((category) => <button key={category.id}>{category.title}</button>)
+            }
+        </div>
+    )
 }
 
 export default Filter
